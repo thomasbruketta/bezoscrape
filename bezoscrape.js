a05e0a2dda8f1371aa924798d21195f98ab6b22b
Thomas Bruketta <thomas.bruketta@azure-ThomasBruketta-osx.local>
2023-09-17 21:39:24 -0700

    yargs me matey

--- bezoscrape.js
+++ bezoscrape.js
@@ -1,16 +1,38 @@
 #!/usr/bin/env node
 
-import { format } from "date-fns";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 import { getPageData } from "./getPageData.js";
 import { processPageData } from "./processPageData.js";
+
+const argv = yargs(hideBin(process.argv))
+    .option("u", {
+        alias: "url",
+        describe: "Amazon product search URL",
+        demandOption:
+            "You must provide a valid Amazon product search URL using the -u or --url option.",
+        type: "string",
+    })
+    .example(
+        "$0 -u https://www.amazon.com/s?k=laptop",
+        "Scrape Amazon search results for laptops."
+    )
+    .help()
+    .alias("h", "help")
+    .fail((msg, err, yargs) => {
+        if (err) throw err; // Preserve stack
+        console.error("Error:", msg);
+        console.error("Run with --help for usage information.");
+        process.exit(1);
+    }).argv;
+
+const amazonUrl = argv.url;
 
 // create an async try catch block to handle errors
 async function scrape() {
     try {
         // call getPageData function
-        await getPageData(
-            "https://www.amazon.com/s?k=headphones&crid=VS7GDL0WY0ZR&sprefix=headphones%2Caps%2C522&ref=nb_sb_noss_2"
-        ).then((pageDataArr) => {
+        await getPageData(amazonUrl).then((pageDataArr) => {
             const result = processPageData(pageDataArr);
 
             console.log(result);
